1f69014561c771e23082bad5574dc29898addcc9
Phil Schools <phil@glencadia.com>
2026-07-28 23:00:20 -0400

    LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,29 @@ class HTMLNode:
             f"stringified='{self.props_to_html()}' \n"
             f")"
             )
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str | None, props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+
+    def __repr__(self) -> str:
+        return (
+            f"{type(self).__name__}( \n"
+            f"tag={self.tag!r}, \n"
+            f"value={self.value!r}, \n"
+            f"props={self.props} \n"
+            f"stringified='{self.props_to_html()}' \n"
+            f")"
+            )
+
+    def to_html(self) -> str:
+        tag = self.tag
+        value = self.value
+        props = self.props
+
+        if props is not None:
+            props_string = self.props_to_html()
+            if tag == "a":
+                return f"<{tag}{props_string}>{value}</{tag}>"
+
+        return f"<{tag}>{value}</{tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -21,3 +21,17 @@ class TestHTMLNode(unittest.TestCase):
         stringified_should_not_be = '"href"="https://google.com"  "target"="_blank"'
         node_to_props = html_node.props_to_html()
         self.assertNotEqual(node_to_props, stringified_should_not_be)
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Google It", {"href": "https://google.com", "target": "_blank"})
+        self.assertNotEqual(node.to_html(), "<a>Google It</a>")
+        self.assertEqual(node.to_html(), '<a href="https://google.com" target="_blank">Google It</a>')
+
+    def test_leaf_to_html_h1(self):
+        node = LeafNode("h1", "Hello, world!")
+        self.assertEqual(node.to_html(), "<h1>Hello, world!</h1>")
