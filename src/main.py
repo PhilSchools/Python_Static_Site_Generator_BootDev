03f2d8f2b0b881f3d38ab2b99b50c5979021033c
Phil Schools <phil@glencadia.com>
2026-09-01 00:21:18 -0400

    gitignored public/

--- src/main.py
+++ src/main.py
@@ -1,17 +1,7 @@
-from classes.htmlnode import HTMLNode
 
 
 def main():
-
-    # fetchski = TextNode("This is the Fetchski Website", TextType.LINK_TEXT, "https://fetchski.com")
-
-    # print(repr(fetchski))
-
-    html = HTMLNode("a", "This is a Link", None, {"href": "https://link.com", "target": "_blank"})
-    html_propped = html.props_to_html()
-
-    print(f"{html!r} \n\n")
-    print(html_propped)
+    print("Hello World")
 
 
 
